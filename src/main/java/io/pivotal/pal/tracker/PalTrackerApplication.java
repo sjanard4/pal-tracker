76a8e4167f3bbcbae07a0011162bea1121d837e6
pal_user <sjanard4@ford.com>
2019-08-07 09:06:02 -0600

    jdbc template 2

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -5,15 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
 
     @Bean
-    public TimeEntryRepository getTimeEntryRepository(){
-
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+    public TimeEntryRepository getTimeEntryRepository(DataSource dataSource){
+//
+//        MysqlDataSource dataSource = new MysqlDataSource();
+//        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
 
         return  new JdbcTimeEntryRepository(dataSource);
     }
